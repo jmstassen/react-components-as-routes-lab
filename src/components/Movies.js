c4ca95d6083d415693e52640bd01855fe82f2440
John M. Stassen <john.stassen@gmail.com>
2021-09-26 15:45:38 -0700

    commit tree 48389d71f2cf58df3e3b49c487a9eea5a63c0755

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+          <div>
+            <h2>Title: {movie.title}</h2>
+            <strong>Time: {movie.time} minutes</strong>
+            <ul><strong>Genre(s):</strong>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>
+        )})}
     </div>
   );
 };
